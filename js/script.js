3aff396d816d9523a762bdfa30c33c5259f2ca0f
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2019-10-30 23:32:23 -0400

    Mobile&tablet resp, almost / img swap done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
 let windowW = window.innerWidth;
-let windowX = window.matchMedia("(max-width: 420px)");
 let windowH = window.innerHeight;
-let toTopButton = document.getElementById("toTopButton");
-let navLinks = document.querySelector(".shopLinks");
-let icon = document.getElementById("icon");
-let header = document.querySelector(".header");
+let windowX = window.matchMedia("(max-width: 420px)");
 
-let toyLink = document.getElementById("toysLink");
-let foodLink = document.getElementById("foodLink");
-let clothesLink = document.getElementById("clotheLink");
-let walkLink = document.getElementById("walkLink");
-let groomLink = document.getElementById("groomLink");
+let icon = document.getElementById("icon");
 
 window.onscroll = function() {
 	scroll();
 };
 
 function scroll() {
+	let toTopButton = document.getElementById("toTopButton");
+
 	if (
 		document.body.scrollTop > 200 ||
 		document.documentElement.scrollTop > 200
@@ -33,6 +27,8 @@ function toTop() {
 }
 
 function openMenu() {
+	let header = document.querySelector(".header");
+
 	if (header.className === ".header") {
 		header.className += " responsive";
 	} else {
@@ -40,14 +36,54 @@ function openMenu() {
 	}
 }
 
-function linksToIcons() {
+function linksToIcons(windowX) {
+	let navLinks = document.querySelector(".shopLinks");
+	let toyLink = document.getElementById("toysLink");
+	let foodLink = document.getElementById("foodLink");
+	let clothesLink = document.getElementById("clotheLink");
+	let walkLink = document.getElementById("walkLink");
+	let groomLink = document.getElementById("groomLink");
+
 	if (windowX.matches) {
 		toyLink.innerHTML = '<i class="fas fa-bone"></i>';
 		foodLink.innerHTML = '<i class="fas fa-utensils"></i>';
 		clothesLink.innerHTML = '<i class="fas fa-tshirt"></i>';
 		walkLink.innerHTML = '<i class="fas fa-walking"></i>';
 		groomLink.innerHTML = '<i class="fas fa-cut"></i>';
+	} else {
+		navLinks.innerHTML = `
+    <a href="" id="toysLink">Toys & Entertainment</a>
+    <a href="" id="foodLink">Food & Treats</a>
+    <a href="" id="clotheLink">Clothing</a>
+    <a href="" id="walkLink">Dog Walking</a>
+    <a href="" id="groomLink">Dog Grooming</a>`;
 	}
 }
-linksToIcons();
 windowX.addListener(linksToIcons);
+linksToIcons(windowX);
+
+function swapWeenie() {
+	let weenies = document.querySelectorAll(".weenie > img");
+	let weenieTarget = document.querySelector(".weenie-selected > img");
+
+	let toyPics = [
+		"Assets/Images/pet-toy-1.jpg",
+		"Assets/Images/pet-toy-2.jpg",
+		"Assets/Images/pet-toy-3.jpg",
+		"Assets/Images/pet-toy-4.jpg",
+		"Assets/Images/pet-toy-5.jpg",
+		"Assets/Images/pet-toy-6.jpg"
+	];
+
+	weenieTarget.src = toyPics[0];
+
+	weenies.forEach(e => {
+		e.addEventListener("click", function() {
+      weenieTarget.src = event.target.src;
+		});
+  });
+  
+
+}
+
+swapWeenie();
